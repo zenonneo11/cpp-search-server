117ab2481f7d70d5c1cf824f271c108d191ca4e1
zenonneo11 <101691712+zenonneo11@users.noreply.github.com>
2023-03-29 09:25:38 +0600

    Update remove_duplicates.cpp

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -1,27 +1,23 @@
 #include <algorithm>
 #include <iostream>
 #include <set>
+#include <utility>
 #include "remove_duplicates.h"
 
 using namespace std;
 
-bool map_key_compare(const map<string, double>& lhs, const map<string, double>& rhs) {
-    return lhs.size() == rhs.size() && equal(lhs.begin(), lhs.end(), rhs.begin(), 
-        [] (auto a, auto b) { return a.first == b.first; });
-}
-
-
 void RemoveDuplicates(SearchServer& search_server) {
-  set<int> id_to_erase;
-  for (auto it1 = search_server.begin(), ite1 = search_server.end(); next(it1)!=ite1; ++it1) {
-      for (auto it2 = next(it1); it2!=ite1; ++it2){
-          auto m1 = search_server.GetWordFrequencies(*it1);
-          auto m2 = search_server.GetWordFrequencies(*it2);          
-          if (map_key_compare(m1,m2)){
-              id_to_erase.insert(*it2);
-
-          }      
-      }      
+    set<int> id_to_erase;
+    set<set<string>> set_of_docs_words;
+    for (auto id: search_server) {
+        set<string> doc_words;
+        for (const auto& [word,_]: search_server.GetWordFrequencies(id)){
+            doc_words.insert(word);     
+        }
+        auto [_, is_inserted] = set_of_docs_words.insert(move(doc_words));
+        if (!is_inserted){
+            id_to_erase.insert(id);
+        }    
     }
     for (auto id: id_to_erase){
         std::cout<<"Found duplicate document id "<<id<<std::endl;
